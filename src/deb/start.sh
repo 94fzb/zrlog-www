86866d2566dfea3f67d1ae68eeaa3e0143fdf558
xiaochun <xchun90@163.com>
2020-07-28 22:15:42 +0800

    Optimize page

--- src/deb/start.sh
+++ src/deb/start.sh
@@ -2,7 +2,8 @@
 cd /var/lib/zrlogwww
 
 if [ -f "sim.pid" ]; then
-nohup kill -9 $(cat sim.pid) &
+kill -9 $(cat sim.pid)
+sleep 1
 fi
 nohup java -Xms8m -Xmx128m -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=log/ -XX:+PrintStringTableStatistics -Dfile.encoding=UTF-8 -jar com.zrlog.jar > nohup.out 2>&1 &
 echo "zrlog is run"
