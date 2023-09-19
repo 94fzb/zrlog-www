727ea107ee390f81d08933e099e713c0e59cdab6
xiaochun <xchun90@163.com>
2023-09-19 21:28:49 +0800

    fix

--- bin/sync.sh
+++ bin/sync.sh
@@ -1,7 +1,7 @@
 syncFolder=${4}
 #upload
-cd ${syncFolder}
 bash -e bin/download-ossutil.sh || true
-./ossutil config -i ${1} -k ${2} -e oss-accelerate.aliyuncs.com
-./ossutil cp -f ${4} oss://${3}/${4}
+cd ${syncFolder}
+../ossutil config -i ${1} -k ${2} -e oss-accelerate.aliyuncs.com
+../ossutil cp -f ${4} oss://${3}/${4}
 
