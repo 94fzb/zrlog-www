db68bbd3c00634f8ab4bc74b8c42338e74427bcc
xiaochun <xchun90@163.com>
2024-07-09 21:04:30 +0800

    add html for pages

--- bin/package-deb.sh
+++
@@ -1,5 +0,0 @@
-./mvnw -U clean compile assembly:single
-cp target/com.zrlog.jar .
-./mvnw -U package -Dassembly.skipAssembly=true -Pgenerator-static
-debName=zrlog-www.deb
-cp target/zrlog-www*.deb $debName
--- bin/upload.sh
+++ bin/upload.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
-sudo apt update
-sudo apt install expect markdown -y
-./mvnw -U clean compile assembly:single -Pgenerator-static
-java -jar target/generator-html.jar
-replace=""
-find static/changelog -name "*.md" -type f -exec bash -c 'markdown "$0" > "${0/.md/$replace}.html"' {} \;
-
+sh bin/generator-static.sh
 sh bin/sync.sh ${1} ${2} ${3} $(pwd)/static/
