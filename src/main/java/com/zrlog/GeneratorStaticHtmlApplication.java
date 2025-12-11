1fb45b565a26858904f094ccc059820031f0b84c
xiaochun <90xchun@gmail.com>
2025-12-11 20:57:51 +0800

    Release 3.2.1

--- src/main/java/com/zrlog/GeneratorStaticHtmlApplication.java
+++ src/main/java/com/zrlog/GeneratorStaticHtmlApplication.java
@@ -15,11 +15,11 @@ public class GeneratorStaticHtmlApplication {
         SimpleWebServer server = Application.server(0);
         new GeneratorHtml("/changelog", PathUtil.getStaticPath() + "/changelog/index.html", server.getApplicationContext(), Application.zrlogServerConfig).run();
         new GeneratorHtml("/download", PathUtil.getStaticPath() + "/download.html", server.getApplicationContext(), Application.zrlogServerConfig).run();
-        new GeneratorHtml("/plugin/", PathUtil.getStaticPath() + "/plugin/index.html", server.getApplicationContext(), Application.zrlogServerConfig).run();
+        new GeneratorHtml("/plugin", PathUtil.getStaticPath() + "/plugin/index.html", server.getApplicationContext(), Application.zrlogServerConfig).run();
         new PluginDAO().findAll().forEach(e -> {
             new GeneratorHtml("/plugin/" + e.getId() + ".html", PathUtil.getStaticPath() + "/plugin/" + e.getId() + ".html", server.getApplicationContext(), Application.zrlogServerConfig).run();
         });
-        new GeneratorHtml("/template/", PathUtil.getStaticPath() + "/template/index.html", server.getApplicationContext(), Application.zrlogServerConfig).run();
+        new GeneratorHtml("/template", PathUtil.getStaticPath() + "/template/index.html", server.getApplicationContext(), Application.zrlogServerConfig).run();
         new TemplateDAO().findAll().forEach(e -> {
             new GeneratorHtml("/template/" + e.getId() + ".html", PathUtil.getStaticPath() + "/template/" + e.getId() + ".html", server.getApplicationContext(), Application.zrlogServerConfig).run();
         });
